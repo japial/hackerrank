89fccecb0febf759867054f160f8eb15159b3f1e
jahangirpial <jahangiralompial@gmail.com>
2020-04-24 13:26:52 +0600

    List Comprehesion Applied

--- Python/list_comprehensions.py
+++ Python/list_comprehensions.py
@@ -2,14 +2,19 @@ x = int(input())
 y = int(input())
 z = int(input())
 n = int(input())
-ar = []
-p = 0
-for i in range(x + 1):
-    for j in range(y + 1):
-        for k in range(z + 1):
-            if i + j + k != n:
-                ar.append([])
-                ar[p] = [i, j, k]
-                p += 1
-print(ar)
+
+# Comprehension Way
+print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)])
+
+# Normal Way
+# ar = []
+# p = 0
+# for i in range(x + 1):
+#     for j in range(y + 1):
+#         for k in range(z + 1):
+#             if i + j + k != n:
+#                 ar.append([])
+#                 ar[p] = [i, j, k]
+#                 p += 1
+# print(ar)
 
