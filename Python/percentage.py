d08fbac1bb28d75d12c1044c15ed9f0338e5c127
jahangirpial <jahangiralompial@gmail.com>
2020-06-16 01:42:07 +0600

    Percentage Decimal Point

---
+++ Python/percentage.py
@@ -0,0 +1,10 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+
+query_name = input()
+average = sum(student_marks[query_name])/len(student_marks[query_name])
+print("%.2f" % average)
